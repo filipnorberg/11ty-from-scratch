b7f625880b7a9677fb3899b2ac5e377a5f0e45be
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2021-10-01 08:49:29 +0200

    implemented meta info and rss

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 
 module.exports = config => {
   // Add filters
@@ -11,6 +13,9 @@ module.exports = config => {
 
   // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
+
+  // Plugins
+    config.addPlugin(rssPlugin);
 
         // Returns work items, sorted by display order
     config.addCollection('work', collection => {
